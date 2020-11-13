906935df64b06f696058013e6bd0c28d0a94f98d
Ramawadh Pal <Ramawadh.Pal@swisscom.com>
2020-11-13 13:55:08 +0100

    Initial commit

---
+++ src/test/java/com/ramawadh/learning/hotelroomreservation/HotelRoomReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramawadh.learning.hotelroomreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
